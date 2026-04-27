10f16ead0871e8a4a20772bce61466f234b5cfda
An orin <99235237+AnorinTool@users.noreply.github.com>
2026-04-27 10:12:58 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,36 +1,29 @@
+cat > $PREFIX/bin/tools_php << 'EOF'
 #!/data/data/com.termux/files/usr/bin/bash
 
-# ===== CONFIG =====
 URL="https://raw.githubusercontent.com/AnorinTool/tools_php/main/code.php"
 TMP="/data/data/com.termux/files/home/.tools_php_tmp.php"
 
 clear
 echo "===== [TOOL PHP SYSTEM BY AN ORIN] ====="
-echo ""
 
-# ===== CHECK PHP =====
 if ! command -v php >/dev/null 2>&1; then
-    echo "[+] Đang cài PHP..."
     pkg update -y && pkg install php -y
 fi
 
-# ===== LOAD CODE =====
 echo "[+] Đang tải tool..."
 
 curl -L -s "$URL" > "$TMP"
 
-# kiểm tra file tải
 if [ ! -s "$TMP" ]; then
-    echo "❌ Lỗi tải dữ liệu (file rỗng hoặc lỗi mạng)"
+    echo "❌ Lỗi tải dữ liệu"
     rm -f "$TMP"
     exit 1
 fi
 
-# ===== FIX CRLF =====
 tr -d '\r' < "$TMP" > "${TMP}_fix"
 
-# ===== EXECUTE =====
 php "${TMP}_fix"
 
-# ===== CLEAN =====
 rm -f "$TMP" "${TMP}_fix"
+EOF
