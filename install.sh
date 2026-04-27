9f36d28862a9e16ef0d145856665857f7b600bf0
An orin <99235237+AnorinTool@users.noreply.github.com>
2026-04-27 09:43:41 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,32 +3,24 @@
 clear
 echo "=== INSTALL TOOL PHP SYSTEM ==="
 
-# 1. Kiểm tra PHP
+# Check PHP
 if ! command -v php >/dev/null 2>&1; then
     echo "[+] Đang cài PHP..."
     pkg update -y && pkg install php -y
 fi
 
-# 2. Tải tool chính (RAW)
-URL="https://5g142.wiremockapi.cloud/shtool/tools_php_main"
+URL="https://raw.githubusercontent.com/AnorinTool/tools_php/main/tools_php.sh"
 DEST="$PREFIX/bin/tools_php"
 
 echo "[+] Đang tải tool..."
 
-curl -L -s "$URL" -o "$DEST"
+curl -L "$URL" -o "$DEST"
 
-# 3. Fix lỗi CRLF (rất quan trọng)
+# FIX CRLF
 sed -i 's/\r$//' "$DEST"
 
-# 4. Cấp quyền
 chmod +x "$DEST"
 
-# 5. Kiểm tra tồn tại
-if [ ! -f "$DEST" ]; then
-    echo "❌ Tải thất bại"
-    exit 1
-fi
-
 echo ""
-echo "✅ Cài đặt hoàn tất"
+echo "✅ Cài đặt hoàn tất!"
 echo "👉 Gõ: tools_php"
