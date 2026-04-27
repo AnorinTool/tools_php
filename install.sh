5bbafd3618cb40cd15db104c6dca282b63211ecd
An orin <99235237+AnorinTool@users.noreply.github.com>
2026-04-27 11:10:39 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-rm -f $PREFIX/bin/tools_php
-
 cat > $PREFIX/bin/tools_php << 'EOF'
-#!/data/data/com.termux/files/usr/bin/bash
+#!/usr/bin/env bash
 
 URL="https://raw.githubusercontent.com/AnorinTool/tools_php/refs/heads/main/code.php"
 TMP="$HOME/.tools_php_tmp.sh"
@@ -18,15 +16,10 @@ if [ ! -s "$TMP" ]; then
     exit 1
 fi
 
-# fix CRLF
 tr -d '\r' < "$TMP" > "${TMP}_fix"
-
 chmod +x "${TMP}_fix"
 
 bash "${TMP}_fix"
 
 rm -f "$TMP" "${TMP}_fix"
 EOF
-
-chmod +x $PREFIX/bin/tools_php
-hash -r
