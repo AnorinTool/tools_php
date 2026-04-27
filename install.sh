942ff06cecfca3cce162adf83e97ab4b7e713489
An orin <99235237+AnorinTool@users.noreply.github.com>
2026-04-27 09:56:34 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,26 +1,2 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-clear
-echo "=== INSTALL TOOL PHP SYSTEM ==="
-
-# Check PHP
-if ! command -v php >/dev/null 2>&1; then
-    echo "[+] Đang cài PHP..."
-    pkg update -y && pkg install php -y
-fi
-
-URL="https://raw.githubusercontent.com/AnorinTool/tools_php/refs/heads/main/code.php"
-DEST="$PREFIX/bin/tools_php"
-
-echo "[+] Đang tải tool..."
-
-curl -L "$URL" -o "$DEST"
-
-# FIX CRLF
-sed -i 's/\r$//' "$DEST"
-
-chmod +x "$DEST"
-
-echo ""
-echo "✅ Cài đặt hoàn tất!"
-echo "👉 Gõ: tools_php"
+tr -d '\r' < /data/data/com.termux/files/usr/bin/tools_php > tmp && mv tmp /data/data/com.termux/files/usr/bin/tools_php
+chmod +x /data/data/com.termux/files/usr/bin/tools_php
